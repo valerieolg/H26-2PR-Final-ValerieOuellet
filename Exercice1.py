fd377739c205ecf5050de981a55bb9aea3112d57
valerieolg <valerieolg@gmail.com>
2026-01-15 14:04:17 -0500

    Commit #2

--- Exercice1.py
+++ Exercice1.py
@@ -1,7 +1,29 @@
-n = int(input("Saisissez un nombre entier: "))
+class Triangle:
+    def __init__(self, nombre, symbol='*'):
+        self.nombre = nombre
+        self.symbol = symbol
 
-symbol = '*'
-space = " "
-for i in range(0,n):
-    print(f"{(n-1)*space}{symbol} {symbol}")
-    symbol = symbol + '*'
+    def lignes(self):
+        lignes = []
+        for i in range(self.nombre):
+            espaces = " " * (self.nombre - 1)
+            etoiles = self.symbol * (i + 1)
+            lignes.append(espaces + etoiles)
+        return lignes
+
+
+class Affichage:
+    def __init__(self, triangle):
+        self.triangle = triangle
+
+    def afficher(self):
+        lignes = self.triangle.lignes()
+        for ligne in lignes:
+            print(ligne + " " + ligne.strip())
+
+
+n = int(input("Saisissez un nombre entier : "))
+
+triangle = Triangle(n)
+affichage = Affichage(triangle)
+affichage.afficher()
